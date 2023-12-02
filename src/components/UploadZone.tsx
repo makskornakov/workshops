1b5658bce29769e3579505001f373153ff70bac6
Maksim Kornakov <makskornakov@ya.ru>
2023-12-02 22:34:08 +0100

    some shit to change the picture on the page

--- src/components/UploadZone.tsx
+++ src/components/UploadZone.tsx
@@ -1,25 +1,22 @@
 'use client';
-
-// You need to import our styles for the button to look right. Best to import in the root /layout.tsx but this is fine
 import '@uploadthing/react/styles.css';
-
 import { UploadDropzone } from '../app/utils/uploadathing';
 
 export function UploadDnD() {
   return (
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <UploadDropzone
       endpoint="profilePicture"
       onClientUploadComplete={(res) => {
         // Do something with the response
         console.log('Files: ', res);
-        alert('Upload Completed');
+        alert('Picture changed successfully!');
+        // ! Change this to something normal
+        window.location.reload();
       }}
       onUploadError={(error: Error) => {
         // Do something with the error.
         alert(`ERROR! ${error.message}`);
       }}
     />
-    // </main>
   );
 }
