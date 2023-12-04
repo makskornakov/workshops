1e2c3ef736f139026cec7fcdc717fd147e63a0d8
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-12-04 15:17:18 +0400

    Update next.config.js
    Co-Authored-By: Maksim Kornakov <makskornakov@gmail.com>

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const withLinaria = require('next-with-linaria');
 /** @type {import('next-with-linaria').LinariaConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com', 'utfs.io'],
+    remotePatterns: [
+      { hostname: 'lh3.googleusercontent.com'},
+      { hostname: 'avatars.githubusercontent.com' },
+      { hostname: 'utfs.io' }, // вроде utfs.io это и есть uploadthing
+      { hostname: 'uploadthing.com' },
+    ],
   },
   // transpilePackages: ['@auth/prisma-adapter'],
 };
