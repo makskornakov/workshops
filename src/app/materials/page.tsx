e105657276b3215f29cb9689d1ebe451d4f6f941
Maksim Kornakov <makskornakov@ya.ru>
2023-12-09 03:41:21 +0100

    material libriary

--- src/app/materials/page.tsx
+++ src/app/materials/page.tsx
@@ -4,13 +4,21 @@ import Link from 'next/link';
 import { StyledButton, StyledLink } from '~/components/NavBar.styled';
 import { getUser } from '../utils/prismaUser';
 import { redirect } from 'next/navigation';
+import { Material } from '@prisma/client';
 
 export default async function MaterialLibrary() {
   // get first 10 materials
 
-  const materials = await prisma.material.findMany({
+  const materials = (await prisma.material.findMany({
     take: 10,
-  });
+    include: {
+      author: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  })) as (Material & { author: { name: string } })[];
   // console.log(materials);
 
   return (
@@ -41,13 +49,53 @@ export default async function MaterialLibrary() {
           <StyledButton type="submit">Create</StyledButton>
         </form>
       </PageHeading>
-      <ul>
+      <div
+        style={{
+          display: 'grid',
+          margin: '2rem',
+          gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
+          gap: '1rem',
+        }}
+      >
         {materials.map((material) => (
-          <li key={material.id}>
-            <Link href={`/materials/${material.id}`}>{material.title}</Link>
-          </li>
+          <MaterialCard key={material.id} material={material} />
         ))}
-      </ul>
+      </div>
     </>
   );
 }
+
+export const MaterialCard = ({
+  material,
+}: {
+  material: Material & { author: { name: string } };
+}) => {
+  return (
+    <Link
+      href={`/materials/${material.id}`}
+      style={{
+        position: 'relative',
+        border: '1px solid #a6a6a6',
+        borderRadius: '.5rem',
+        padding: '1rem',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1rem',
+        alignItems: 'flex-start',
+      }}
+    >
+      <h3
+        style={{
+          fontSize: '1.2rem',
+          fontWeight: 400,
+        }}
+      >
+        {material.title}
+      </h3>
+      {material.description && <p>{material.description}</p>}
+      <p style={{ color: '#a6a6a6' }}>
+        {material.author.name} - {new Date(material.createdAt).toLocaleDateString()}
+      </p>
+    </Link>
+  );
+};
