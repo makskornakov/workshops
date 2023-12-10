ecb55065b41db914c147132cea7bc7ccb9a6135b
Maksim Kornakov <makskornakov@ya.ru>
2023-12-10 20:06:30 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,6 @@
-// import Image from 'next/image';
-// import { LoginButton, LogoutButton, ProfileButton, RegisterButton } from '../components/buttons';
-
-// import { Main } from './page.styled';
-// import prisma from '../../lib/prisma';
-// import MagicLinkEmail from './utils/magicLink';
 import { PageHeading } from './profile/profile.styled';
 
-// import { getServerSession } from 'next-auth';
-// import { authOptions } from '../../lib/auth';
-
-// const allUsers = async () => {
-//   const users = await prisma.user.findMany();
-//   return users;
-// };
-
 export default async function Home() {
-  // const session = await getServerSession(authOptions);
-  // console.log(session);
-
-  // const users = await allUsers();
   return (
     <>
       <PageHeading>Home Page</PageHeading>
