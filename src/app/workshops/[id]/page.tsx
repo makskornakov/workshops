dbe61d764570ad1d0dbaef159f9ac1c99f66779c
Maksim Kornakov <makskornakov@ya.ru>
2023-12-10 20:26:15 +0100

    lets merge

--- src/app/workshops/[id]/page.tsx
+++ src/app/workshops/[id]/page.tsx
@@ -3,6 +3,7 @@ import { getUser } from '../../../utils/prismaUser';
 import WorkshopForm from '../WorkshopForm';
 
 export default async function Workshops({ params }: { params: { id: string } }) {
+  redirect('/'); // ? Workshop page is not ready yet
   const user = await getUser();
 
   if (!user) {
--- src/app/workshops/page.tsx
+++ src/app/workshops/page.tsx
@@ -3,6 +3,7 @@ import { getUser } from '../../utils/prismaUser';
 import WorkshopForm from './WorkshopForm';
 
 export default async function Workshops() {
+  redirect('/'); // ? Workshop page is not ready yet
   const user = await getUser();
 
   if (!user) {
